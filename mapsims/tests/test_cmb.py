5a3cdcaf85e6d3398510900e56d3f47ca11ec7aa
Mathew Madhavacheril <mathewsyriac@gmail.com>
2018-12-05 19:47:17 -0800

    test script for loading cmb sim

---
+++ mapsims/tests/test_cmb.py
@@ -0,0 +1,17 @@
+from pixell import enmap
+import numpy as np
+import os,sys
+import healpy as hp
+from mapsims import cmb # FIXME: relative path import?
+
+def test_load_sim():
+    nside = 32
+    # Make an IQU sim
+    imap = cmb.get_cmb_sky(iteration_num=0,nside = nside)
+    assert imap.shape[0]==3
+    # Make an I only sim
+    imap = cmb.get_cmb_sky(iteration_num=0,nside = nside)
+    assert imap.ndim==1
+
+# FIXME: Remove the explicit call below once py.test is working
+test_load_sim()
