af966d7318c1f09aefcc0476d3d83af2d30c5212
Mathew Madhavacheril <mathewsyriac@gmail.com>
2018-12-20 16:14:14 -0800

    black formatting

--- mapsims/cmb/__init__.py
+++ mapsims/cmb/__init__.py
@@ -1,21 +1,23 @@
 import healpy as hp, numpy as np
 import so_pysm_models as spm
 
-def get_cmb_sky(iteration_num, 
-                nside = None, #set this if healpix is desired
-                shape = None, #set shape and wcs if CAR maps are desired
-                wcs = None,
-                lensed = True,
-                aberrated = False,
-                modulation = False,
-                has_polarization = True,
-                cmb_set = 0, #We allow for more than one CMB map per lensing map
-                cmb_dir = None, 
-                nu = None,
-                input_units="uK_RJ",
-                pixel_indices = None
+
+def get_cmb_sky(
+    iteration_num,
+    nside=None,  # set this if healpix is desired
+    shape=None,  # set shape and wcs if CAR maps are desired
+    wcs=None,
+    lensed=True,
+    aberrated=False,
+    modulation=False,
+    has_polarization=True,
+    cmb_set=0,  # We allow for more than one CMB map per lensing map
+    cmb_dir=None,
+    nu=None,
+    input_units="uK_RJ",
+    pixel_indices=None,
 ):
-    '''
+    """
     Return a CMB map from stored alm's.  This can be in Healpix format
     (if nside is specified) or rectangular pixel format (if wcs and shape are
     specified).  The lensed alm's are pre-stored.
@@ -41,33 +43,41 @@ def get_cmb_sky(iteration_num,
         output: (nfreqs,ncomp,npix) healpix array if nside is specified, else
         returns (nfreqs,ncomp,Ny,Nx) rectangular pixel ndmap.
         
-    '''
+    """
     ncomp = 3 if has_polarization else 1
-    filename = _get_cmb_map_string(cmb_dir,iteration_num,cmb_set,lensed,aberrated)
-    #The hdu = (1, 2,3) means get all of T, E, B
-    #Note the alm's are stored as complex32, so upgrade this for processing
+    filename = _get_cmb_map_string(cmb_dir, iteration_num, cmb_set, lensed, aberrated)
+    # The hdu = (1, 2,3) means get all of T, E, B
+    # Note the alm's are stored as complex32, so upgrade this for processing
 
-    sgen = spm.PrecomputedAlms(filename,
-                               target_nside = nside, 
-                               target_shape = shape,
-                               target_wcs = wcs,
-                               input_units=input_units,
-                               has_polarization=has_polarization, pixel_indices=pixel_indices)
+    sgen = spm.PrecomputedAlms(
+        filename,
+        target_nside=nside,
+        target_shape=shape,
+        target_wcs=wcs,
+        input_units=input_units,
+        has_polarization=has_polarization,
+        pixel_indices=pixel_indices,
+    )
 
-    return sgen.signal(nu=nu,modulation=modulation)
+    return sgen.signal(nu=nu, modulation=modulation)
 
-def  _get_default_cmb_directory():
-    #FIXME: remove hard-coding to use preferred directory path system
+
+def _get_default_cmb_directory():
+    # FIXME: remove hard-coding to use preferred directory path system
     return "/global/project/projectdirs/sobs/v4_sims/mbs/cmb"
 
-def _get_cmb_map_string(cmb_dir,iteration_num,cmb_set,lensed,aberrated):
+
+def _get_cmb_map_string(cmb_dir, iteration_num, cmb_set, lensed, aberrated):
     # Implements the CMB lensed alms file naming convention
     # Ideally the same function should be used when saving sims
-    if cmb_dir is None: cmb_dir = _get_default_cmb_directory()
+    if cmb_dir is None:
+        cmb_dir = _get_default_cmb_directory()
     lstring = "Lensed" if lensed else "Unlensed"
     abstring = "Abberated" if aberrated else "Unabberated"
-    cmb_map_type = "%s%sCMB" % (lstring,abstring)
-    filename = cmb_dir + "/fullsky%s_alm_set%02d_%05d.fits" % ( cmb_map_type, cmb_set , iteration_num)
+    cmb_map_type = "%s%sCMB" % (lstring, abstring)
+    filename = cmb_dir + "/fullsky%s_alm_set%02d_%05d.fits" % (
+        cmb_map_type,
+        cmb_set,
+        iteration_num,
+    )
     return filename
-
-    
--- mapsims/tests/test_cmb.py
+++ mapsims/tests/test_cmb.py
@@ -1,19 +1,28 @@
 import numpy as np
-import os,sys
+import os, sys
 import healpy as hp
-from mapsims import cmb # FIXME: relative path import?
+from mapsims import cmb  # FIXME: relative path import?
 from astropy.utils.data import get_pkg_data_filename
 
+
 def test_load_sim():
-    cmb_dir = 'mapsims/data' # FIXME: how do I get path to data?
-    save_name = 'mapsims/data/test_map.fits'
+    cmb_dir = "mapsims/data"  # FIXME: how do I get path to data?
+    save_name = "mapsims/data/test_map.fits"
     nside = 32
     # Make an IQU sim
-    imap = cmb.get_cmb_sky(iteration_num=0,nside = nside,cmb_dir=cmb_dir,lensed=False,aberrated=False)
-    imap_test = hp.read_map(save_name,field=(0,1,2))
+    imap = cmb.get_cmb_sky(
+        iteration_num=0, nside=nside, cmb_dir=cmb_dir, lensed=False, aberrated=False
+    )
+    imap_test = hp.read_map(save_name, field=(0, 1, 2))
     np.testing.assert_allclose(imap, imap_test)
-    assert imap.shape[0]==3
+    assert imap.shape[0] == 3
     # Make an I only sim
-    imap = cmb.get_cmb_sky(iteration_num=0,nside = nside, has_polarization=False,cmb_dir=cmb_dir,lensed=False,aberrated=False)
-    assert imap.ndim==2
-
+    imap = cmb.get_cmb_sky(
+        iteration_num=0,
+        nside=nside,
+        has_polarization=False,
+        cmb_dir=cmb_dir,
+        lensed=False,
+        aberrated=False,
+    )
+    assert imap.ndim == 2
