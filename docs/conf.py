1992fb5f1a8f85e7489aa67b81f2feb14c326144
Andrea Zonca <code@andreazonca.com>
2019-09-23 09:55:07 -0700

    intersphinx with so_pysm_models 2.0.dev

--- docs/conf.py
+++ docs/conf.py
@@ -198,5 +198,5 @@ github_issues_url = "https://github.com/{0}/issues/".format(setup_cfg["github_pr
 #     target = target.strip()
 #     nitpick_ignore.append((dtype, six.u(target)))
 intersphinx_mapping = {
-    "so_pysm_models": ("https://so-pysm-models.readthedocs.io/en/latest", None)
+    "so_pysm_models": ("https://so-pysm-models.readthedocs.io/en/2.0.dev", None)
 }
