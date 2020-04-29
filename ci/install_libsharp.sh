1051c8665ec7d6dbe75671ea0082fae0a5ab3e00
Mathew Madhavacheril <mathewsyriac@gmail.com>
2020-04-29 15:16:04 -0400

    check if removing mpi in libsharp helps

--- ci/install_libsharp.sh
+++ ci/install_libsharp.sh
@@ -3,9 +3,9 @@ git clone https://github.com/Libsharp/libsharp --branch v1.0.0 --single-branch -
     && cd libsharp \
     && autoreconf \
     && CC="mpicc" \
-    ./configure --enable-mpi --enable-pic \
+    ./configure --enable-pic \
     && make -j4 \
     && cp -a auto/* $LIBSHARP \
     && cd python \
-    && CC="mpicc -g" LDSHARED="mpicc -g -shared" \
+    && CC="gcc -g" LDSHARED="gcc -g -shared" \
     pip install .
