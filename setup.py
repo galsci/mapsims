29a16056fcd9f6e41f6c4faf82473f0472eea6e0
Andrea Zonca <code@andreazonca.com>
2020-05-21 18:11:51 -0700

    Revert "test: update to newer astropy-helpers"
    This reverts commit 97e67949cb76f874341a2155b5182bc2e4e6d4b8.

--- setup.py
+++ setup.py
@@ -13,25 +13,17 @@ from setuptools.config import read_configuration
 from astropy_helpers.setup_helpers import register_commands, get_package_info
 from astropy_helpers.version_helpers import generate_version_py
 
-conf = read_configuration("setup.cfg")
 # Store the package name in a built-in variable so it's easy
 # to get from other parts of the setup infrastructure
-builtins._ASTROPY_PACKAGE_NAME_ = conf["metadata"]["name"]
+builtins._ASTROPY_PACKAGE_NAME_ = read_configuration('setup.cfg')['metadata']['name']
 
 # Create a dictionary with setup command overrides. Note that this gets
 # information about the package (name and version) from the setup.cfg file.
-try:
-    cmdclass = register_commands(
-        conf["metadata"]["name"],
-        conf["metadata"]["version"],
-        conf["metadata"]["version"],
-    )
-except TypeError as e:
-    raise TypeError("Need updated version of astropy-helpers") from e
+cmdclass = register_commands()
 
 # Freeze build information in version.py. Note that this gets information
 # about the package (name and version) from the setup.cfg file.
-version = generate_version_py(conf["metadata"]["name"], conf["metadata"]["version"])
+version = generate_version_py()
 
 # Get configuration information from all of the various subpackages.
 # See the docstring for setup_helpers.update_package_files for more
