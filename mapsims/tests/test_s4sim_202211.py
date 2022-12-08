7cfa6b7bbf3f807a1300d9d2d0c25261ef4e00df
Andrea Zonca <code@andreazonca.com>
2022-12-08 11:36:10 -0800

    test and input config for S4 simulation

---
+++ mapsims/tests/test_s4sim_202211.py
@@ -0,0 +1,14 @@
+import healpy as hp
+from numpy.testing import assert_allclose
+
+from ..runner import command_line_script
+
+def test_s4sim_202222_ame_high():
+    command_line_script(["mapsims/tests/data/common.toml", "mapsims/tests/data/ame_high.toml",
+        "--verbose", "--channels", 'LFL1', "--nside", "16"])
+
+    output_map = hp.read_map("cmbs4_ame_high_uKCMB_LAT-LFL1_nside64_0000.fits", (0,1,2))
+
+    expected_map = hp.read_map("mapsims/tests/data/cmbs4_ame_high_uKCMB_LAT-LFL1_nside64_0000.fits.gz", (0,1,2))
+
+    assert_allclose(expected_map, output_map)
