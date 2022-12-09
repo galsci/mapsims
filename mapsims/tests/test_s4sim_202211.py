01d7cc493ad60aa00e39387d23bf04833c53c28b
Andrea Zonca <code@andreazonca.com>
2022-12-09 10:40:45 -0800

    use nside 16

--- mapsims/tests/test_s4sim_202211.py
+++ mapsims/tests/test_s4sim_202211.py
@@ -7,8 +7,8 @@ def test_s4sim_202222_ame_high():
     command_line_script(["mapsims/tests/data/common.toml", "mapsims/tests/data/ame_high.toml",
         "--verbose", "--channels", 'LFL1', "--nside", "16"])
 
-    output_map = hp.read_map("cmbs4_ame_high_uKCMB_LAT-LFL1_nside64_0000.fits", (0,1,2))
+    output_map = hp.read_map("cmbs4_ame_high_uKCMB_LAT-LFL1_nside16_0000.fits", (0,1,2))
 
-    expected_map = hp.read_map("mapsims/tests/data/cmbs4_ame_high_uKCMB_LAT-LFL1_nside64_0000.fits.gz", (0,1,2))
+    expected_map = hp.read_map("mapsims/tests/data/cmbs4_ame_high_uKCMB_LAT-LFL1_nside16_0000.fits.gz", (0,1,2))
 
     assert_allclose(expected_map, output_map)
