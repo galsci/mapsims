139105f98e9593a6cb99bcff74158cdd5f581067
Andrea Zonca <code@andreazonca.com>
2023-01-20 12:45:54 -0800

    fix: log.warn is deprecated

--- mapsims/utils.py
+++ mapsims/utils.py
@@ -75,12 +75,12 @@ class RemoteData:
         for folder in self.data_folders:
             full_path = os.path.join(folder, filename)
             if os.path.exists(full_path):
-                log.warn(f"Access data from {full_path}")
+                log.warning(f"Access data from {full_path}")
                 return full_path
         with data.conf.set_temp("dataurl", self.data_url), data.conf.set_temp(
             "remote_timeout", 90
         ):
-            log.warn(f"Retrieve data for {filename} (if not cached already)")
+            log.warning(f"Retrieve data for {filename} (if not cached already)")
             map_out = data.get_pkg_data_filename(filename, show_progress=True)
         return map_out
 
